1d7e8bdfe610804b6f6332d50e77261b583f96aa
aashishrupreja <aashish.rupreja4797@gmail.com>
2023-05-18 10:33:38 +0530

    Complete sketch logic
    User can draw on sketchpad by holding down left mouse button if it is
    released drawing will be stopped.

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,16 +1,28 @@
 const button = document.getElementById("draw");
 const container = document.getElementById("container");
 
-const gridSize = prompt("Enter size");
+let items;
 
-button.addEventListener("click", draw)
+const gridSize = 16;
 
-function draw(e) {
-    for (let i = 1; i <= (gridSize * gridSize); i++) {
-        const item = document.createElement("div");
-        item.setAttribute("class", "container-items");
-        item.style.width = `${(container.clientWidth) / gridSize}px`;
-        item.style.height = `${(container.clientHeight) / gridSize}px`;
-        container.appendChild(item);
-    }
+for (let i = 1; i <= (gridSize * gridSize); i++) {
+    const item = document.createElement("div");
+    item.setAttribute("class", "container-items");
+    item.style.width = `${(container.clientWidth) / gridSize}px`;
+    item.style.height = `${(container.clientHeight) / gridSize}px`;
+    container.appendChild(item);
 }
+container.addEventListener("mousedown", containerEvent => {
+    items = Array.from(document.getElementsByClassName("container-items"));
+    items.forEach(item => item.addEventListener("mouseover", color));
+});
+container.addEventListener("mouseup", containerEvent => {
+    items.forEach(item => item.removeEventListener("mouseover", color));
+});
+
+function color(e) {
+    this.classList.add("colored");
+}
+
+
+
